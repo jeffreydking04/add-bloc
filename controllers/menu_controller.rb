98cc9b61f42e7cf5cfb90184e954798fda865a47
Jeffrey King <jdking33@aol.com>
2016-10-18 13:01:07 -0700

    Finish this

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -100,9 +100,23 @@ class MenuController
   end
 
   def read_csv
-    puts "Enter file name"
+    print "Enter file name: "
     file_name = gets.chomp
-    address_book.import_from_csv(file_name)
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+      entry_count = address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
   end
 
   def entry_submenu(entry)
@@ -116,6 +130,10 @@ class MenuController
     case selection
     when "n"
     when "d"
+      delete_entry(entry)
+    when "e"
+      edit_entry(entry)
+      entry_submenu(entry)
     when "m"
       system "clear"
       main_menu
@@ -130,5 +148,25 @@ class MenuController
     system "clear"
     puts address_book.entries[index].to_s
     entry_submenu(address_book.entries[index])
+  end
+
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def edit_entry(entry)
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets. chomp
+    print "Updated email: "
+    email = gets.chomp
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+    puts "Updated entry: "
+    puts entry.to_s
   end
 end
